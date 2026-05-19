e3a9655d21ae8dcd7fa29cf3885522017fdeada5
Fabian Marzan <fabi.marzan1762@yahoo.com>
2026-05-18 22:14:56 -0400

    fix(npc): support travel cost discounts, |TRAVELCOST| parsing, and keyword conditions. - Fixes travel NPC |TRAVELCOST| placeholder not parsing in messages. - Fixes KeywordHandler and KeywordNode ignoring 'condition' and 'action' arguments.

--- data/npc/lib/crystalcompat/keywordhandler.lua
+++ data/npc/lib/crystalcompat/keywordhandler.lua
@@ -13,3 +13,51 @@ function CrystalKeywordHandler:new(...)
 	setmetatable(obj, CrystalKeywordHandler)
 	return obj
 end
+
+-- Extension to support condition and action in the Keyword Node and Handler
+if KeywordNode then
+	function KeywordNode:new(keys, func, param, condition, action)
+		local obj = {}
+		obj.keywords = keys
+		obj.callback = func
+		obj.parameters = param
+		obj.condition = condition
+		obj.action = action
+		obj.children = {}
+		setmetatable(obj, self)
+		self.__index = self
+		return obj
+	end
+
+	function KeywordNode:processMessage(cid, message)
+		if self.condition then
+			local player = Player(cid)
+			if player then
+				if not self.condition(player) then
+					return false
+				end
+			end
+		end
+
+		if self.action then
+			local player = Player(cid)
+			if player then
+				self.action(player)
+			end
+		end
+
+		return (not self.callback or
+			       self.callback(cid, message, self.keywords, self.parameters, self))
+	end
+
+	function KeywordNode:addChildKeyword(keywords, callback, parameters, condition, action)
+		local new = KeywordNode:new(keywords, callback, parameters, condition, action)
+		return self:addChildKeywordNode(new)
+	end
+end
+
+if KeywordHandler then
+	function KeywordHandler:addKeyword(keys, callback, parameters, condition, action)
+		return self:getRoot():addChildKeyword(keys, callback, parameters, condition, action)
+	end
+end
--- data/npc/lib/crystalcompat/stdmodule.lua
+++ data/npc/lib/crystalcompat/stdmodule.lua
@@ -15,6 +15,34 @@ local function crystalParseText(npcHandler, text, parseInfo)
 	return npcHandler:parseMessage(text, parseInfo)
 end
 
+function CrystalStdModule.getTravelCost(player, baseCost, discount)
+	local cost = baseCost or 0
+	if not discount then
+		return cost
+	end
+
+	local discounts = {}
+	if type(discount) == "table" then
+		discounts = discount
+	elseif type(discount) == "string" then
+		discounts = { discount }
+	end
+
+	for _, d in ipairs(discounts) do
+		if d == "postman" then
+			if player:getStorageValue(Storage.Quest.U7_24.ThePostmanMissions.Rank) == 5 then
+				cost = cost - 10
+			end
+		elseif d == "new frontier" then
+			if player:getStorageValue(Storage.Quest.U8_54.TheNewFrontier.Mission10[1]) == 2 then
+				cost = 0
+			end
+		end
+	end
+
+	return math.max(0, cost)
+end
+
 function CrystalStdModule.say(...)
 	if _G.CrystalCompatDebug then
 		_G.CrystalCompatDebug("StdModule.say called")
@@ -53,6 +81,12 @@ function CrystalStdModule.say(...)
 		[TAG_PLAYERNAME] = player:getName(),
 	}
 
+	if parameters.cost then
+		local cost = type(parameters.cost) == "function" and parameters.cost(player) or parameters.cost
+		cost = CrystalStdModule.getTravelCost(player, cost, parameters.discount)
+		parseInfo["|TRAVELCOST|"] = cost
+	end
+
 	local text = parameters.text or parameters.message
 	local parsedText = crystalParseText(npcHandler, text, parseInfo)
 	local delay = parameters.delay or parameters.interval or 4000
@@ -70,6 +104,74 @@ function CrystalStdModule.say(...)
 	return true
 end
 
+function CrystalStdModule.travel(...)
+	local argc = select("#", ...)
+	local cid, message, keywords, parameters, node
+
+	-- Supports both old-style callback shape and the native NpcSystem shape.
+	if argc >= 6 then
+		local npc, creature, msgType, msg, keys, params = ...
+		cid = type(creature) == "number" and creature or (Player(creature) and Player(creature):getId() or 0)
+		message = msg
+		keywords = keys
+		parameters = params
+		node = select(7, ...)
+	else
+		cid, message, keywords, parameters, node = ...
+	end
+
+	local npcHandler = parameters and parameters.npcHandler or nil
+	if npcHandler == nil then
+		error("StdModule.travel called without any npcHandler instance.")
+	end
+
+	if not npcHandler:isFocused(cid) then
+		return false
+	end
+
+	local player = Player(cid)
+	if not player then
+		return false
+	end
+
+	if player:isPremium() or not parameters.premium then
+		if player:isPzLocked() then
+			npcHandler:say("First get rid of those blood stains! You are not going to ruin my vehicle!", cid)
+		elseif parameters.level and player:getLevel() < parameters.level then
+			npcHandler:say("You must reach level " .. parameters.level .. " before I can let you go there.", cid)
+		else
+			local cost = parameters.cost or 0
+			if type(cost) == "function" then
+				cost = cost(player)
+			end
+			cost = CrystalStdModule.getTravelCost(player, cost, parameters.discount)
+
+			if not player:removeTotalMoney(cost) then
+				npcHandler:say("You don't have enough money.", cid)
+			else
+				npcHandler:say(parameters.msg or "Set the sails!", cid)
+				npcHandler:releaseFocus(cid)
+
+				local destination = parameters.destination
+				if type(destination) == "function" then
+					destination = destination(player)
+				end
+				local destinationPos = Position(destination)
+				local position = player:getPosition()
+				player:teleportTo(destinationPos)
+
+				position:sendMagicEffect(CONST_ME_TELEPORT)
+				destinationPos:sendMagicEffect(CONST_ME_TELEPORT)
+			end
+		end
+	else
+		npcHandler:say("I'm sorry, but you need a premium account in order to travel onboard our ships.", cid)
+	end
+	npcHandler:resetNpc(cid)
+	return true
+end
+
 if StdModule then
 	StdModule.say = CrystalStdModule.say
+	StdModule.travel = CrystalStdModule.travel
 end
